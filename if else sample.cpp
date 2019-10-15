60b5f00f43d5dfedbd91a4160e7fd4181a8213f9
gurkan-kaya <56433592+gurkan-kaya@users.noreply.github.com>
2019-10-15 17:32:01 +0300

    Add files via upload

---
+++ if else sample.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream> 
+#include <cstdlib>
+#include <locale.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int puan;
+	cout << "Puanı gir:";
+	cin >> puan;
+	if (puan >= 85)
+		cout << "Puan 85'ten büyük ve yeterli \n";
+	else
+		cout << "Puan 85'ten küçük ve yetersiz \n";
+
+}

3d3df63a6d4e72d1618230e415a1f0d266f40dd6
gurkan-kaya <56433592+gurkan-kaya@users.noreply.github.com>
2019-11-07 20:21:24 +0300

    Eskenar Dortgen Bastirma

---
+++ Eskenar Dortgen.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Bir tek sayi giriniz:";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int y, b;
+		if (i <= n / 2) {
+			y = 2 * i + 1;
+		}
+		else
+		{
+			y = 2*(n - i-1) + 1;
+		}
+		b = (n - y) / 2;
+		for (int j = 0; j < b; j++)
+			cout << " ";
+		for (int j = 0; j <y; j++) {
+
+			cout << "*";
+         }
+		cout << endl;
+
+	
+	
+	}
+
+		
+
+	}
+
+

7fe49ed4251b364dbb0327c9ae1fd7711e3160b3
gurkan-kaya <56433592+gurkan-kaya@users.noreply.github.com>
2020-07-27 15:36:11 +0300

    Add files via upload

---
+++ overloading_input_output_operators.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+	int real, imag;
+
+public:
+	Complex(int r = 0, int i = 0)
+	{
+		real = r;
+		imag = i;
+	}
+
+	friend ostream& operator<< (ostream& ekran, Complex &c1);
+	friend istream& operator>> (istream& klavye, Complex &c1);
+
+};
+
+ostream& operator << (ostream& ekran, Complex& c1)
+{
+	ekran << c1.real;
+	ekran << "+i" << c1.imag << endl;
+	return ekran;
+}
+
+istream& operator >> (istream& klavye, Complex& c1)
+{
+	cout << "Enter real part";
+	klavye >> c1.real;
+	cout << "Enter imagenory part";
+	klavye >> c1.imag  ;
+		return klavye;
+}
+
+int main()
+{
+	Complex c1;
+	cin >> c1;
+	cout << "The complex object is ";
+	cout << c1;
+	return 0;
+}
